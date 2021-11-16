849e8d4a1b2d62dc133b13b780fce0d92167f260
Bulat <bulat0talipov@gmail.com>
2021-11-16 20:34:19 +0300

    added coment 2

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
 $ (function (){
-  
+  $('.header__btn').on('click', function () {
+    $('.header__btn-box').toggleClass('header__menu-btn--active');
+    $('.menu').toggleClass('menu__active');
+  })
+
+  var mixer = mixitup('.cases__content');
+
+  $('.consultation-form__box-label').on('click', function () {
+    $('.consultation-form__box-label').removeClass('consultation-form__box-label--active');
+    $(this).addClass('consultation-form__box-label--active');
+  })
+
+
+  $('.reviews__items').slick({
+    centerMode: true,
+    slidesToShow: 1,
+    dots: true,
+    arrows: false
+  });
+
+
 });
